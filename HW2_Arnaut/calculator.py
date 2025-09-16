cffb79a91c7785b0c54c42c48369caf5e94efa4d
Oleg <arnautoleg1@gmail.com>
2025-09-16 09:50:29 +0300

    Add main() function

--- HW2_Arnaut/calculator.py
+++ HW2_Arnaut/calculator.py
@@ -1,1 +1,23 @@
 
+
+
+
+
+
+
+
+def main():
+    expression = input()
+    elements = expression.split()
+    a = Decimal(elements[0])
+    b = Decimal(elements[2])
+    if elements[1] == '-':
+        result = minus(a, b)
+    elif elements[1] == '+':
+        result = plus(a, b)
+    elif elements[1] == '*':
+        result = product(a, b)
+    elif (elements[1] == '/'):
+        result = divide(a, b)
+    return result
+
